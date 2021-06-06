44ce40a75732d99358c8799bef3157a32af915f1
selendenizkizilirmak <85319357+selendenizkizilirmak@users.noreply.github.com>
2021-06-06 04:58:37 +0300

    Add files via upload

---
+++ answers.R
@@ -0,0 +1,15 @@
+library(dplyr)
+
+data = read.csv("country_vaccination_stats.csv", header = T)
+head(data)
+summary(data)
+sum(is.na(data))
+
+data <- data %>%
+  mutate(daily_vaccinations  = ifelse(is.na(daily_vaccinations), min(na.omit(daily_vaccinations)), daily_vaccinations))
+
+sum(is.na(data))
+
+
+data = data %>% mutate(vaccines = as.factor(vaccines))
+
